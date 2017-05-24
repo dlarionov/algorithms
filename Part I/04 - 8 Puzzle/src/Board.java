a5655eb40b8a9fcc3dec6135935f798bf686079c
dlarionov <denis.larionov@gmail.com>
2017-05-24 21:15:15 +0300

    started Solver

--- Part I/04 - 8 Puzzle/src/Board.java
+++ Part I/04 - 8 Puzzle/src/Board.java
@@ -1,28 +1,44 @@
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Stack;
+import java.util.Arrays;
 
 public class Board 
 {
     private int n;
-    private int[] arr;
+    private int[][] arr;
     private int z;
+    private int hamming;
+    private int manhattan;
     
-    // construct a board from an n-by-n array of blocks (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks)
     {
+        if (blocks == null)
+            throw new java.lang.NullPointerException();
+        
         n = blocks.length;
-        int size = n*n;
-        arr = new int[size];
+        arr = new int[n][n];
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
                 int k = i*n+j;
-                arr[k] = blocks[i][j];
-                if (arr[k] == 0)
+                int x = blocks[i][j];
+                if (x == 0) 
                     z = k;
+                else if (x != k+1)
+                {
+                    hamming++;
+                    manhattan+= distance(k+1, x);
+                }
+                arr[i][j] = x;
             }
         }
+    }
+    
+    private int distance(int goal, int value)
+    {
+        int dif = Math.abs(goal - value);
+        return dif % n + dif / n;
     }
     
     public int dimension()
@@ -32,111 +48,97 @@ public class Board
     
     public int hamming() 
     {
-        int cnt = 0;
-        for(int i = 1; i <= arr.length; i++)
-        {
-            int j = arr[i-1];
-            if (j == 0 || j == i)
-                continue;
-            cnt++;
-        }
-        return cnt;
+        return hamming;
     }
     
     public int manhattan() 
     {
-        int sum = 0;
-        for(int i = 1; i <= arr.length; i++)
-        {
-            int j = arr[i-1];
-            if (j == 0 || j == i)
-                continue;
-            sum = sum + distance(i, j);
-        }
-        return sum;
-    }
-    
-    private int distance(int goal, int value)
-    {
-        int dif = Math.abs(goal - value);
-        return dif % n + dif / n;
+        return manhattan;
     }
     
     public boolean isGoal() 
     {
-        return hamming() == 0;
+        return hamming == 0;
     }
     
-    // TODO avoid zero
     public Board twin()
     {
-        int[][] blocks = getCopy();
-        int temp = blocks[0][0];
-        blocks[0][0] = blocks[0][n-1];
-        blocks[0][n-1] = temp;
-        return new Board(blocks);        
-    }
-    
-    private int[][] getCopy()
-    {
-        int[][] copy = new int[n][n];
-        for (int i = 0; i < arr.length; i++)
+        if (n < 2)
+            return new Board(arr);
+        
+        Board board;
+        if (z < n)
         {
-            copy[i/n][i%n] = arr[i];
+            int x = arr[1][0];
+            arr[1][0] = arr[1][1];
+            arr[1][1] = x;
+            board = new Board(arr);
+            arr[1][1] = arr[1][0];
+            arr[1][0] = x;            
         }
-        return copy;
-    }
+        else
+        {
+            int x = arr[0][0];
+            arr[0][0] = arr[0][1];
+            arr[0][1] = x;
+            board = new Board(arr);
+            arr[0][1] = arr[0][0];
+            arr[0][0] = x;            
+        }        
+        return board;        
+    }    
     
-    // TODO something wrong
     public boolean equals(Object y)
     {
         if (y == this) return true;
         if (y == null) return false;
         if (y.getClass() != this.getClass()) return false;
         Board that = (Board) y;
-        return this.n == that.n;
+        return this.n == that.n && Arrays.equals(this.arr, that.arr); // WTF !?
     }
     
-    public Iterable<Board> neighbors() 
+    public Iterable<Board> neighbors()
     { 
         Stack<Board> stack = new Stack<Board>();
-        int i = z/n, j = z%n;
-        int[][] copy = getCopy();
+        if (n < 2)
+            return stack;
         
+        int i = z/n, j = z%n;
+                
         if (i > 0) // top
         {
-            copy[i][j] = copy[i-1][j];
-            copy[i-1][j] = 0;
-            stack.push(new Board(copy));
-            copy[i-1][j] = copy[i][j];
-            copy[i][j] = 0;
+            arr[i][j] = arr[i-1][j];
+            arr[i-1][j] = 0;
+            stack.push(new Board(arr));
+            arr[i-1][j] = arr[i][j];
+            arr[i][j] = 0;
         }
         
         if (i < n-1) // bottom
         {
-            copy[i][j] = copy[i+1][j];
-            copy[i+1][j] = 0;
-            stack.push(new Board(copy));
-            copy[i+1][j] = copy[i][j];
-            copy[i][j] = 0;
+            arr[i][j] = arr[i+1][j];
+            arr[i+1][j] = 0;
+            stack.push(new Board(arr));
+            arr[i+1][j] = arr[i][j];
+            arr[i][j] = 0;
         }
         
         if (j < n-1) // right
         {
-            copy[i][j] = copy[i][j+1];
-            copy[i][j+1] = 0;
-            stack.push(new Board(copy));
-            copy[j][j+1] = copy[i][j];
-            copy[i][j] = 0;
+            arr[i][j] = arr[i][j+1];
+            arr[i][j+1] = 0;
+            stack.push(new Board(arr));
+            arr[j][j+1] = arr[i][j];
+            arr[i][j] = 0;
         }
         
         if (j > 0) // left
         {
-            copy[i][j] = copy[i][j-1];
-            copy[i][j-1] = 0;
-            stack.push(new Board(copy));
-            copy[i][j-1] = copy[i][j];
-            copy[i][j] = 0;
+            arr[i][j] = arr[i][j-1];
+            arr[i][j-1] = 0;
+            stack.push(new Board(arr));
+            arr[i][j-1] = arr[i][j];
+            arr[i][j] = 0;
         }  
         
         return stack;
@@ -150,7 +152,7 @@ public class Board
         {
             for (int j = 0; j < n; j++) 
             {
-                s.append(String.format("%2d ", arr[i*n+j]));
+                s.append(String.format("%2d ", arr[i][j]));
             }
             s.append("\n");
         }
@@ -160,8 +162,8 @@ public class Board
     public static void main(String[] args)
     {
         Board board = new Board(new int[][] { {8, 1, 3}, {4, 0, 2}, {7, 6, 5} });
-        int h = board.hamming();
-        int m = board.manhattan();
-        StdOut.println(h + " " + m);
+        StdOut.print(board);
+        StdOut.println(board.hamming());
+        StdOut.println(board.manhattan());
     }
 }
--- Part I/04 - 8 Puzzle/src/Solver.java
+++ Part I/04 - 8 Puzzle/src/Solver.java
@@ -1,7 +1,133 @@
-public class Solver {
-    public Solver(Board initial)           // find a solution to the initial board (using the A* algorithm)
-    public boolean isSolvable()            // is the initial board solvable?
-    public int moves()                     // min number of moves to solve initial board; -1 if unsolvable
-    public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
-    public static void main(String[] args) // solve a slider puzzle (given below)
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+
+public class Solver 
+{
+    private boolean isSolvable;
+    private Transform solution;
+    private MinPQ<Transform> pq;
+    
+    public Solver(Board initial)
+    {
+        if (initial == null)
+            throw new java.lang.NullPointerException();
+        
+        Transform root = new Transform(initial, 0, null);
+        pq.insert(root);        
+        move(root);
+        
+        while(!pq.isEmpty())
+        {
+            Transform t = pq.delMin();
+            Board b = t.board();
+            if (b.isGoal())
+            {
+                solution = t;
+                break;
+            }
+            move(t);
+        }
+    }
+    
+    private void move(Transform prev)
+    {
+        int age = prev.age()+1;
+        for(Board board : prev.board.neighbors())
+        {
+            pq.insert(new Transform(board, age, prev));
+        }
+    }
+    
+    private class Transform implements Comparable<Transform>
+    {
+        private Board board;
+        private int age;
+        private Transform prev;
+        
+        public Transform(Board board, int age, Transform prev)
+        {
+            this.board =  board;
+            this.age = age;
+            this.prev = prev;
+        }
+        
+        public int compareTo(Transform that)
+        { 
+            return this.priority() - that.priority();
+        }
+        
+        public int priority()
+        {
+            return this.board.manhattan() + this.age;
+        }
+        
+        public Board board()
+        {
+            return this.board;
+        }
+        
+        public int age()
+        {
+            return this.age;
+        }
+        
+        public Transform prev()
+        {
+            return this.prev;
+        }
+    }
+    
+    public boolean isSolvable()
+    {
+        return isSolvable;
+    }
+    
+    public int moves()
+    {
+        if (!isSolvable)
+            return -1;
+        
+        return solution.age();
+    }
+    
+    public Iterable<Board> solution()
+    {
+        if (!isSolvable)
+            return null;
+        
+        Stack<Board> stack = new Stack<Board>();
+        Transform x = solution;
+        while(x != null)
+        {
+            stack.push(x.board());
+            x = x.prev();
+        }
+        return stack;
+    }
+    
+    public static void main(String[] args)
+    {
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+            blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+        
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+        
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+    }
 }
