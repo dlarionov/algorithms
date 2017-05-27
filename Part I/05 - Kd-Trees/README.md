15c428d670d99fc76e58825587c100aa6f19ba52
Denis <denis.larionov@gmail.com>
2017-05-27 23:04:39 +0300

    started with PointSET

---
+++ Part I/05 - Kd-Trees/README.md
@@ -0,0 +1,4 @@
+## Programming Assignment 5: Kd-Trees
+
+[http://coursera.cs.princeton.edu/algs4/assignments/kdtree.html](http://coursera.cs.princeton.edu/algs4/assignments/kdtree.html)
+[http://coursera.cs.princeton.edu/algs4/checklists/kdtree.html](http://coursera.cs.princeton.edu/algs4/checklists/kdtree.html)
---
+++ Part I/05 - Kd-Trees/src/PointSET.java
@@ -0,0 +1,74 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+public class PointSET {
+    SET<Point2D> set;
+    
+    public PointSET() {
+        set = new SET<Point2D>();
+    }
+    
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
+    
+    public int size() {
+        return set.size();
+    }
+    
+    public void insert(Point2D p) {
+        if (p == null)
+            throw new java.lang.NullPointerException();
+        
+        if (!set.contains(p))
+            set.add(p);
+    }
+    
+    public boolean contains(Point2D p) {
+        return set.contains(p);
+    }
+    
+    public void draw() {
+        for (Point2D p : set)
+            p.draw();
+    }
+    
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null)
+            throw new java.lang.NullPointerException();
+        
+        Stack<Point2D> arr = new Stack<Point2D>();
+        for (Point2D p : set)
+        {
+            if (rect.contains(p))
+                arr.push(p);            
+        }
+        return arr;
+    }
+    
+    public Point2D nearest(Point2D p) {
+        if (p == null)
+            throw new java.lang.NullPointerException();
+        
+        if (set.isEmpty())
+            return null;
+        
+        Point2D champion = null;
+        double distance = 2;
+        for (Point2D q : set) {
+            double d = p.distanceTo(q);
+            if (Double.compare(distance, d) > 0) {
+                champion = q;
+                distance = d;
+            }
+        }
+        return champion;
+    }        
+    
+    public static void main(String[] args) {
+        
+    }
+}
