d014991f0d41773c966c0706d2724d2b1df324af
dlarionov <denis.larionov@gmail.com>
2017-06-05 15:32:40 +0300

    Successor with delete

---
+++ Part I/IQ/01 - Union-Find/Successor with delete/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            QuickUnion qu = new QuickUnion(n);
+            Random rnd = new Random();
+            for (int i = 0; i < 3*n; i++)
+            {
+                qu.Remove(rnd.Next(0, n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"successor for {i} is {qu.Successor(i)}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Part I/IQ/01 - Union-Find/Successor with delete/ConsoleApp1/QuickUnion.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class QuickUnion
+    {
+        int[] _arr;
+
+        public QuickUnion(int n)
+        {
+            if (n < 2)
+                throw new ArgumentException();
+
+            // array with one additional element
+            _arr = new int[n + 1];
+            for (int i = 0; i <= n; i++)
+            {
+                _arr[i] = i;
+            }
+        }
+
+        public void Remove(int x)
+        {
+            if (x < 0 || x > _arr.Length - 2)
+                throw new ArgumentException();
+
+            Union(x, x + 1);
+        }
+
+        public int Successor(int x)
+        {
+            int root = Root(x);
+            return root > _arr.Length - 2 ? -1 : root;
+        }
+
+        private int Root(int i)
+        {
+            int j = i;
+            while (_arr[j] != j)
+            {
+                j = _arr[j];
+            }
+
+            // path compression
+            while (_arr[i] != i)
+            {
+                i = _arr[i];
+                _arr[i] = j;
+            }
+
+            return j;
+        }
+
+        public bool Connected(int a, int b)
+        {
+            int i = Root(a);
+            int j = Root(b);
+
+            return i == j;
+        }
+
+        public void Union(int a, int b)
+        {
+            int i = Root(a);
+            int j = Root(b);
+
+            if (i == j)
+                return;
+
+            // parent always greather then child
+            _arr[i] = j;
+        }
+    }
+}
