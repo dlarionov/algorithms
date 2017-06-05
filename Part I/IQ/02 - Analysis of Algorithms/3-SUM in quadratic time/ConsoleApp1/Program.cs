5f230c1e19315800cc0d5959591ce3ebbefb0c00
dlarionov <denis.larionov@gmail.com>
2017-06-05 19:10:40 +0300

    test 3sum

--- Part I/IQ/02 - Analysis of Algorithms/3-SUM in quadratic time/ConsoleApp1/Program.cs
+++ Part I/IQ/02 - Analysis of Algorithms/3-SUM in quadratic time/ConsoleApp1/Program.cs
@@ -6,7 +6,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+            int[] arr = { -7, -5, -4, -3, -3, 0, 1, 2, 3, 4, 5, 6 };
+            // Array.Sort(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int x = arr[i];
+                int lo = 0;
+                int hi = arr.Length - 1;
+
+                while (lo < hi)
+                {
+                    int y = arr[lo];
+                    int z = arr[hi];
+                    int sum = y + z;
+
+                    if (hi == i || sum > x)
+                        hi--;
+                    else if (lo == i || sum < x)
+                        lo++;
+                    else
+                    {
+                        Console.WriteLine($"({arr[lo]},{arr[i]},{arr[hi]})");
+                        break;
+                    }
+                }
+            }
 
             Console.ReadLine();
         }
