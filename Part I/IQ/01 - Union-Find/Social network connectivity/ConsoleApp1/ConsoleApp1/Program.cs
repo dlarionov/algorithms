852325f448fb837cdad67737b4a5cbf4a2c0081e
Denis <denis.larionov@gmail.com>
2017-06-05 09:40:51 +0300

    Social network connectivity

---
+++ Part I/IQ/01 - Union-Find/Social network connectivity/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            bool allConnected = false;
+            Random rnd = new Random();
+            var uf = new WeightedQuickUnion(n);
+            for (int i = 0; i < 3 * n; i++)
+            {
+                int x = rnd.Next(0, n);
+                int y = rnd.Next(0, n);
+
+                if (!uf.Connected(x, y))
+                {
+                    uf.Union(x, y);
+
+                    Console.WriteLine($"({x}, {y}) {uf.Count()}");
+
+                    if (uf.Count() == 1)
+                    {
+                        allConnected = true;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(allConnected);
+            Console.ReadLine();
+        }
+    }
+}
