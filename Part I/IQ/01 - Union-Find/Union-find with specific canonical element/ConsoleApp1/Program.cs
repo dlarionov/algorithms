f86d5d6978fd109f4ce0c29b37977eefe2fa2267
dlarionov <denis.larionov@gmail.com>
2017-06-05 12:26:43 +0300

    Union-find with specific canonical element

---
+++ Part I/IQ/01 - Union-Find/Union-find with specific canonical element/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uf = new WeightedQuickUnion(10);
+            uf.Union(0, 4);
+            uf.Union(2, 5);
+            uf.Union(2, 6);
+            uf.Union(4, 2);
+            uf.Union(1, 7);
+            uf.Union(7, 3);
+            uf.Union(3, 8);
+            uf.Union(9, 3);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"max for {i} is {uf.FindMax(i)}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Part I/IQ/01 - Union-Find/Union-find with specific canonical element/ConsoleApp1/WeightedQuickUnion.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class WeightedQuickUnion
+    {
+        int[] _arr;
+        int[] _sz;
+        int[] _max;
+
+        public WeightedQuickUnion(int n)
+        {
+            if (n < 2)
+                throw new ArgumentException();
+
+            _arr = new int[n];
+            _max = new int[n];
+            _sz = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                _arr[i] = i;
+                _max[i] = i;
+                _sz[i] = 1;
+            }
+        }
+
+        private int Root(int i)
+        {
+            while (_arr[i] != i)
+            {
+                i = _arr[i];
+            }
+            return i;
+        }
+
+        public bool Connected(int a, int b)
+        {
+            int i = Root(a);
+            int j = Root(b);
+
+            return i == j;
+        }
+
+        public void Union(int a, int b)
+        {
+            int i = Root(a);
+            int j = Root(b);
+
+            if (i == j)
+                return;
+
+            if (_sz[i] < _sz[j])
+            {
+                _arr[i] = j;
+                _sz[j] += _sz[i];
+                _max[j] = Math.Max(_max[i], _max[j]);
+            }
+            else
+            {
+                _arr[j] = i;
+                _sz[i] += _sz[j];
+                _max[i] = Math.Max(_max[i], _max[j]);
+            }
+        }
+
+        public int FindMax(int i)
+        {
+            int j = _max[i];
+            while (_arr[i] != i)
+            {
+                i = _arr[i];
+                if (_max[i] > j)
+                    j = _max[i];
+            }
+            return j;
+        }
+    }
+}
