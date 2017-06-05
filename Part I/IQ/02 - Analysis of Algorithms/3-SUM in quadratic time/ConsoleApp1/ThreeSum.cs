910d4d45eec3b3c8490ead1014da1e91402278e4
dlarionov <denis.larionov@gmail.com>
2017-06-05 18:59:45 +0300

    3-SUM in quadratic time

---
+++ Part I/IQ/02 - Analysis of Algorithms/3-SUM in quadratic time/ConsoleApp1/ThreeSum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class ThreeSum
+    {
+        public ThreeSum(int[] arr)
+        {
+            int[] copy = new int[arr.Length];
+            Array.Copy(arr, copy, arr.Length);
+            Array.Sort(copy);
+
+            for (int i = 0; i < copy.Length; i++)
+            {
+                int x = copy[i];
+                int lo = 0;
+                int hi = copy.Length - 1;
+
+                while (lo < hi)
+                {
+                    int y = copy[lo];
+                    int z = copy[hi];
+                    int sum = y + z;
+
+                    if (hi == i || sum > x)
+                        hi--;
+                    else if (lo == i || sum < x)
+                        lo++;
+                    else
+                    {
+                        // TODO
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
