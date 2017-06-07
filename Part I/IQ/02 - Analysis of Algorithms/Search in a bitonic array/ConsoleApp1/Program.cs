eca64fe4afc9f0d82ec30dc552bc77cd1231bb1a
Denis <denis.larionov@gmail.com>
2017-06-07 22:36:06 +0300

    BitonicArray

--- Part I/IQ/02 - Analysis of Algorithms/Search in a bitonic array/ConsoleApp1/Program.cs
+++ Part I/IQ/02 - Analysis of Algorithms/Search in a bitonic array/ConsoleApp1/Program.cs
@@ -9,19 +9,19 @@ namespace ConsoleApp1
         {
             int n = 100000;
             int density = 3;
+            int range = n * density;
             int tries = 20;
+            var rnd = new Random();
 
             // create array of random distinct integers
-            var rand = new Random();
             int[] arr = new int[n];
-            int range = n * density;
             var set = new HashSet<int>();
             for (int i = 0; i < n; i++)
             {
-                int x = rand.Next(range);
+                int x = rnd.Next(range);
                 while (set.Contains(x))
                 {
-                    x = rand.Next(range);
+                    x = rnd.Next(range);
                 }
                 set.Add(x);
             }
@@ -32,15 +32,12 @@ namespace ConsoleApp1
             Array.Sort(arr, 0, mid);
             Array.Sort(arr, mid, n - mid, new ReverseComparer<int>());
 
-            // create bitonic
+            // create and test bitonic
             var bitonic = new BitonicArray(arr);
-
-            // test
             Console.WriteLine($"n={n} density={density} tries={tries}\nx\tn\t3lgn\t2lgn");
-            var rnd = new Random();
             for (int i = 0; i < tries; i++)
             {
-                int x = rnd.Next(n * density);
+                int x = rnd.Next(range);
                 Console.WriteLine($"{x}\t{bitonic.FindLinear(x)}\t{bitonic.FindLogarithmicBad(x)}\t{bitonic.FindLogarithmicGood(x)}");
             }
 
