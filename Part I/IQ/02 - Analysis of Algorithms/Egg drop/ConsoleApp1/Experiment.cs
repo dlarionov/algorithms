8d7c05a56420f61b066076b8d8af0ee8537e61f1
Denis <denis.larionov@gmail.com>
2017-06-09 12:49:23 +0300

    Egg drop version one and two

---
+++ Part I/IQ/02 - Analysis of Algorithms/Egg drop/ConsoleApp1/Experiment.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Experiment
+    {
+        readonly int _height;
+        readonly int _floor;
+
+        public Experiment(int n, int t)
+        {
+            if (n < 1 || t < 1 || t > n)
+                throw new ArgumentException();
+
+            _height = n;
+            _floor = t;
+        }
+
+        private bool Drop(int floor)
+        {
+            return floor >= _floor;
+        }
+
+        public int Version0(int tosses)
+        {
+            int i = 0;
+            while (!Drop(++i))
+            {
+                if (--tosses < 1)
+                    return -1;
+            }
+            return i;
+        }
+
+        public int Version1(int eggs, int tosses)
+        {
+            int lo = 0;
+            int hi = _height;
+            int mid;
+            while (hi - lo > 1)
+            {
+                mid = lo + (hi - lo) / 2;
+                var broken = Drop(mid);
+                if (--tosses < 1)
+                    return -1;
+                if (broken && --eggs < 1)
+                    return -2;
+                if (broken)
+                    hi = mid;
+                else
+                    lo = mid;
+            }
+
+            return hi;
+        }
+    }
+}
---
+++ Part I/IQ/02 - Analysis of Algorithms/Egg drop/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 100;
+            int tries = 100000;
+            var rnd = new Random();
+            int errors = 0;
+
+            for (int j = 0; j < tries; j++)
+            {
+                int n = rnd.Next(1, max);
+                int t = rnd.Next(1, n + 1);
+                int logn = (int)Math.Ceiling(Math.Log(n, 2));
+                int logt = (int)Math.Ceiling(Math.Log(t, 2));
+
+                var ex = new Experiment(n, t);
+                int t0 = ex.Version0(t);
+                int t1 = ex.Version1(logn + 1, logn + 1);
+
+                if (t != t0 || t != t1)
+                {
+                    Console.WriteLine($"{n}\t{t}\t{t0}\t{t1}");
+                    errors++;
+                }
+            }
+
+            Console.WriteLine($"Total tries: {tries}; errors: {errors}\nPress any key...");
+            Console.ReadKey();
+        }
+    }
+}
