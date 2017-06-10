2115f390a3ec3fcde5606235a1b928d90bed7f5c
Denis <denis.larionov@gmail.com>
2017-06-10 12:59:02 +0300

    Stack with max

---
+++ Part I/IQ/03 - Stacks and Queues/Stack with max/ConsoleApp1/MaxStack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class MaxStack
+    {
+        private Stack<int> _arr;
+        private Stack<int> _max;
+
+        public MaxStack()
+        {
+            _arr = new Stack<int>();
+            _max = new Stack<int>();
+        }
+
+        public int Size()
+        {
+            return _arr.Count;
+        }
+
+        public int Max()
+        {
+            return _max.Peek();
+        }
+
+        public void Push(int x)
+        {
+            _arr.Push(x);
+            if (_max.Count > 0)
+            {
+                int last = _max.Peek();
+                _max.Push(x > last ? x : last);
+            }
+            else
+                _max.Push(x);
+        }
+
+        public int Peek()
+        {
+            return _arr.Peek();
+        }
+
+        public int Pop()
+        {
+            _max.Pop();
+            return _arr.Pop();
+        }
+    }
+}
---
+++ Part I/IQ/03 - Stacks and Queues/Stack with max/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = 100;
+            int tries = 10;
+            var stack = new MaxStack();
+            var rnd = new Random();
+            for (int i = 0; i < tries; i++)
+            {
+                int j = rnd.Next(range);
+                stack.Push(j);
+                Console.Write($"push: {j} max: {stack.Max()}\n");
+            }
+            Console.WriteLine();
+
+            while (stack.Size() > 0)
+            {
+                Console.Write($"max: {stack.Max()} pop: {stack.Pop()}\n");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
