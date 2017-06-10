021341bd99ee68d638f842481085f22933dad3de
Denis <denis.larionov@gmail.com>
2017-06-10 08:24:52 +0300

    Queue with two stacks

---
+++ Part I/IQ/03 - Stacks and Queues/Queue with two stacks/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new TwoStackQueue<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                queue.enqueue(i);
+            }
+
+            while (queue.size() > 0)
+            {
+                Console.Write($"{queue.dequeue()} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Part I/IQ/03 - Stacks and Queues/Queue with two stacks/ConsoleApp1/TwoStackQueue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class TwoStackQueue<T>
+    {
+        private Stack<T> _left;
+        private Stack<T> _right;
+
+        public TwoStackQueue()
+        {
+            _left = new Stack<T>();
+            _right = new Stack<T>();
+        }
+
+        public int size()
+        {
+            return _left.Count + _right.Count;
+        }
+
+        public void enqueue(T x)
+        {
+            _right.Push(x);
+        }
+
+        public T dequeue()
+        {
+            if (size() < 1)
+                throw new IndexOutOfRangeException();
+
+            if (_left.Count < 1)
+                while (_right.Count > 0)
+                    _left.Push(_right.Pop());
+
+            return _left.Pop();
+        }
+    }
+}
