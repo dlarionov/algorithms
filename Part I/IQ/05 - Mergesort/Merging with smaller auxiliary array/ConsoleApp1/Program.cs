483930c9cde9bb64f70428a7b664cb92bda79f36
dlarionov <denis.larionov@gmail.com>
2017-06-14 16:00:50 +0300

    Merging with smaller auxiliary array

--- Part I/IQ/05 - Mergesort/Merging with smaller auxiliary array/ConsoleApp1/Program.cs
+++ Part I/IQ/05 - Mergesort/Merging with smaller auxiliary array/ConsoleApp1/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            MergeSort(new int[] { 5, -2, 4, 42, 0, 3, 2, 1, 7 });
+            int[] a = new int[] { 5, -2, 4, 42, 0, 3, 2, 1, 7 };
+            for (int k = 0; k < a.Length; k++)
+                Console.Write($"{a[k]} ");
+            Console.WriteLine();
+
+            MergeSort(a);
+
+            for (int k = 0; k < a.Length; k++)
+                Console.Write($"{a[k]} ");
+            Console.WriteLine();
+
             Console.ReadKey();
         }
 
@@ -23,7 +33,7 @@ namespace ConsoleApp1
             int mid = lo + (hi - lo) / 2;
             Sort(a, aux, lo, mid);
             Sort(a, aux, mid + 1, hi);
-            Merge2(a, aux, lo, mid, hi);
+            Merge3(a, aux, lo, mid, hi);
         }
 
         private static void Merge(int[] a, int[] aux, int lo, int mid, int hi)
@@ -91,6 +101,26 @@ namespace ConsoleApp1
             for (int k = lo; k <= hi; k++)
                 Console.Write($"{a[k]} ");
             Console.WriteLine();
+        }
+
+        private static void Merge3(int[] a, int[] aux, int lo, int mid, int hi)
+        {
+            for (int k = lo; k <= mid; k++)
+                aux[k] = a[k];
+
+            int i = lo;
+            int j = mid + 1;
+            for (int k = lo; k <= hi; k++)
+            {
+                if (i > mid)
+                    a[k] = a[j++];
+                else if (j > hi)
+                    a[k] = aux[i++];
+                else if (a[j] < aux[i])
+                    a[k] = a[j++];
+                else
+                    a[k] = aux[i++];
+            }
         }
     }
 }
