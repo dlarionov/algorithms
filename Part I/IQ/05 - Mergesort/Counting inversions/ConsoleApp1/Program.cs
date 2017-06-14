eaa648404f38ccf14590825680469b26f4dbd47a
Denis <denis.larionov@gmail.com>
2017-06-14 22:54:31 +0300

    Counting inversions

--- Part I/IQ/05 - Mergesort/Counting inversions/ConsoleApp1/Program.cs
+++ Part I/IQ/05 - Mergesort/Counting inversions/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -6,43 +7,55 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[] a = new int[] { 5, -2, 4, 42, 0, 3, 2, 1, 7 };
-            for (int k = 0; k < a.Length; k++)
-                Console.Write($"{a[k]} ");
-            Console.WriteLine();
+            var data = new List<int[]>
+            {
+                new int[] { 1, 2, 3, 4 },
+                new int[] { 1, 2, 4, 3 },
+                new int[] { 2, 1, 4, 3 },
+                new int[] { 4, 1, 2, 3 },
+            };
 
-            MergeSort(a);
+            foreach (var a in data)
+            {
+                for (int k = 0; k < a.Length; k++)
+                    Console.Write($"{a[k]} ");
+                Console.WriteLine();
 
-            for (int k = 0; k < a.Length; k++)
-                Console.Write($"{a[k]} ");
-            Console.WriteLine();
+                int inversions = MergeSort(a);
+
+                for (int k = 0; k < a.Length; k++)
+                    Console.Write($"{a[k]} ");
+                Console.WriteLine($"\ninversions: {inversions}");
+            }
 
             Console.ReadKey();
         }
 
-        public static void MergeSort(int[] a)
+        public static int MergeSort(int[] a)
         {
             int[] aux = new int[a.Length];
-            Sort(a, aux, 0, a.Length - 1);
+            return Sort(a, aux, 0, a.Length - 1);
         }
 
-        private static void Sort(int[] a, int[] aux, int lo, int hi)
+        private static int Sort(int[] a, int[] aux, int lo, int hi)
         {
             if (hi <= lo)
-                return;
+                return 0;
             int mid = lo + (hi - lo) / 2;
-            Sort(a, aux, lo, mid);
-            Sort(a, aux, mid + 1, hi);
-            Merge(a, aux, lo, mid, hi);
+            int left = Sort(a, aux, lo, mid);
+            int right = Sort(a, aux, mid + 1, hi);
+            int self = Merge(a, aux, lo, mid, hi);
+            return left + right + self;
         }
 
-        private static void Merge(int[] a, int[] aux, int lo, int mid, int hi)
+        private static int Merge(int[] a, int[] aux, int lo, int mid, int hi)
         {
             for (int k = lo; k <= hi; k++)
                 aux[k] = a[k];
 
             int i = lo;
             int j = mid + 1;
+            int inversions = 0;
             for (int k = lo; k <= hi; k++)
             {
                 if (i > mid)
@@ -50,10 +63,14 @@ namespace ConsoleApp1
                 else if (j > hi)
                     a[k] = aux[i++];
                 else if (aux[j] < aux[i])
+                {
                     a[k] = aux[j++];
+                    inversions++;
+                }
                 else
                     a[k] = aux[i++];
             }
+            return inversions;
         }
     }
 }
