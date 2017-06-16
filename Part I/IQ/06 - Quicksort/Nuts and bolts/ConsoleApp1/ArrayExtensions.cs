b9d054685acc24c86e01f034bded64e8638bbea9
Denis <denis.larionov@gmail.com>
2017-06-16 20:32:45 +0300

    Nuts and Bolts

---
+++ Part I/IQ/06 - Quicksort/Nuts and bolts/ConsoleApp1/ArrayExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public static class ArrayExtensions
+    {
+        // https://stackoverflow.com/a/110570/1392696
+        public static void Shuffle<T>(this T[] a, Random rnd)
+        {
+            int n = a.Length;
+            while (n > 1)
+            {
+                int k = rnd.Next(n--);
+                a.Swap(n, k);
+            }
+        }
+
+        public static void Swap<T>(this T[] a, int i, int j)
+        {
+            var x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+    }
+}
---
+++ Part I/IQ/06 - Quicksort/Nuts and bolts/ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 16;
+            var rnd = new Random();
+
+            var nuts = new int[n];
+            var bolts = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                nuts[i] = i;
+                bolts[i] = i;
+            }
+
+            nuts.Shuffle(rnd);
+            bolts.Shuffle(rnd);
+
+            //int[] nuts = { 1, 2, 0, 3 };
+            //int[] bolts = { 3, 0, 2, 1 };
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{nuts[i]} {bolts[i]}");
+            }
+            Console.WriteLine();
+
+            PickUp(nuts, bolts, 0, n - 1);
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{nuts[i]} {bolts[i]}");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+        public static void PickUp(int[] nuts, int[] bolts, int lo, int hi)
+        {
+            if (hi <= lo)
+                return;
+
+            int i = Partition(nuts, bolts[lo], lo, hi);
+            int j = Partition(bolts, nuts[i], lo, hi);
+
+            PickUp(nuts, bolts, lo, j - 1);
+            PickUp(nuts, bolts, j + 1, hi);
+        }
+
+        private static int Partition(int[] a, int x, int lo, int hi)
+        {
+            int i = lo, j = hi, k = -1;
+            while (true)
+            {
+                while (a[i] <= x)
+                {
+                    if (k < 0 && a[i] == x)
+                        k = i;
+                    if (i == hi)
+                        break;
+                    i++;
+                }
+
+                while (a[j] > x)
+                {
+                    if (j == lo)
+                        break;
+                    j--;
+                }
+
+                if (i >= j) break;
+                a.Swap(i, j);
+            }
+
+            a.Swap(k, j);
+            return j;
+        }
+    }
+}
