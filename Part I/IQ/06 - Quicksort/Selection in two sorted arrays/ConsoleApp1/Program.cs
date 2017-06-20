cef57057df050b544e084ecf6495392329ebb7c2
dlarionov <denis.larionov@gmail.com>
2017-06-20 18:20:51 +0300

    Selection in two sorted arrays

--- Part I/IQ/06 - Quicksort/Selection in two sorted arrays/ConsoleApp1/Program.cs
+++ Part I/IQ/06 - Quicksort/Selection in two sorted arrays/ConsoleApp1/Program.cs
@@ -6,9 +6,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
-
+            int[] n = { 1, 3, 5, 5, 7, 31 };
+            int[] m = { 0, 0, 1, 1, 1, 2, 4, 7, 7, 42 };
+            int k = FindKth(n, m, 6);
+            Console.WriteLine(k);
             Console.ReadKey();
         }
+
+        public static int FindKth(int[]n, int[]m, int k)
+        {
+            bool isKth(int i) => n[i] >= m[k - i];
+
+            int lo = 0;
+            int hi = n.Length - 1;
+            int mid = lo + (hi - lo) / 2;
+            while (hi - lo > 1) // todo
+            {
+                if (isKth(mid))
+                    lo++;
+                else
+                    hi++;
+            }
+
+            return -1;
+        }
+
+
     }
 }
