770b2cf0193e8e9bc7b235b4a777630b6376d4c5
Denis <denis.larionov@gmail.com>
2017-06-21 09:47:40 +0300

    Selection in two sorted arrays

--- Part I/IQ/06 - Quicksort/Selection in two sorted arrays/ConsoleApp1/Program.cs
+++ Part I/IQ/06 - Quicksort/Selection in two sorted arrays/ConsoleApp1/Program.cs
@@ -7,30 +7,38 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int[] n = { 1, 3, 5, 5, 7, 31 };
-            int[] m = { 0, 0, 1, 1, 1, 2, 4, 7, 7, 42 };
-            int k = FindKth(n, m, 6);
+            int[] m = { 0, 0, 1, 1, 1, 2 };
+            int k = FindKth(n, m, 4);
             Console.WriteLine(k);
             Console.ReadKey();
         }
 
-        public static int FindKth(int[]n, int[]m, int k)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        /// <param name="k">starts from zero</param>
+        /// <returns></returns>
+        public static int FindKth(int[] n, int[] m, int k)
         {
-            bool isKth(int i) => n[i] >= m[k - i];
-
-            int lo = 0;
+            int lo = -1;
             int hi = n.Length - 1;
-            int mid = lo + (hi - lo) / 2;
-            while (hi - lo > 1) // todo
+            int mid;
+
+            while (hi - lo > 1)
             {
-                if (isKth(mid))
-                    lo++;
+                mid = lo + (hi - lo) / 2;
+
+                if (k - mid < 0)
+                    lo = mid;
+                else if (n[mid] >= m[k-mid])
+                    hi = mid;
                 else
-                    hi++;
+                    lo = mid;
             }
 
-            return -1;
+            return n[hi];
         }
-
-
     }
 }
