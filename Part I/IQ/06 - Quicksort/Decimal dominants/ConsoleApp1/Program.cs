aa744d688c5ef00094d3ac2bf62282f2ad57fbd9
Denis <denis.larionov@gmail.com>
2017-06-26 08:03:25 +0300

    Decimail dominants

--- Part I/IQ/06 - Quicksort/Decimal dominants/ConsoleApp1/Program.cs
+++ Part I/IQ/06 - Quicksort/Decimal dominants/ConsoleApp1/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -10,31 +9,24 @@ namespace ConsoleApp1
         {
             var rnd = new Random();
             int tries = 10000;
-            int n = 10000;
-            int k = 100; // kth dominant
-            int d = 100;
+            int n = 1000;
+            int k = 10; // kth dominant
+            int density = 10;
             int errors = 0;
             for (int i = 0; i < tries; i++)
             {
                 int[] a = new int[n];
                 for (int j = 0; j < n; j++)
-                    a[j] = rnd.Next(d);
+                    a[j] = rnd.Next(density);
 
-                int[] calc = KthDominants(a, k);
-                int[] test = KthDominantsBrute(a, k);
+                int[] linear = KthDominants(a, k);
+                int[] brute = KthDominantsBrute(a, k);
 
-                if (!Equals(calc, test))
-                {
-                    //foreach (var j in calc)
-                    //    Console.Write($"{j} ");
-                    //Console.WriteLine();
+                Array.Sort(linear);
+                Array.Sort(brute);
 
-                    //foreach (var j in test)
-                    //    Console.Write($"{j} ");
-                    //Console.WriteLine();
-
+                if (!Equals(linear, brute))
                     errors++;
-                }
             }
             Console.WriteLine($"tries: {tries} errors: {errors}");
             Console.ReadKey();
@@ -42,54 +34,24 @@ namespace ConsoleApp1
 
         public static int[] KthDominantsBrute(int[] arr, int k)
         {
-            var cnt = arr.Length / k;
-            var list = arr.GroupBy(x => x)
-                .Select(x => new { item = x.Key, count = x.Count() })
-                .Where(x => x.count >= cnt)
-                .OrderBy(x => x.item)
-                .ToList();
-
-            //foreach (var i in list)
-            //    Console.Write($"{i.item} {i.count}\n");
-
-            return list.Select(i => i.item).ToArray();
+            int count = arr.Length / k;
+            var list = arr
+                .GroupBy(i => i)
+                .Select(i => new { Key = i.Key, Count = i.Count() })
+                .Where(i => i.Count >= count)
+                .Select(i => i.Key)
+                .ToArray();
+            return list;
         }
 
-        // The trick is that the dominant element remains same if you delete any k distinct items from the array.
         public static int[] KthDominants(int[] arr, int k)
         {
-            // k munus one counters
-            var list = new Dictionary<int, int>();
-            int grade = 1;
+            var data = new Dominants(arr.Length, k);
             for (int i = 0; i < arr.Length; i++)
             {
-                var x = arr[i];
-                if (list.ContainsKey(x))
-                    list[x]++;
-                else
-                {
-                    if (list.Count < k)
-                        list.Add(x, 1);
-                    else
-                    {
-                        foreach (var j in list.Keys.ToArray())
-                        {
-                            if (list[j] > 1)
-                                list[j]--;
-                            else
-                                list.Remove(j);
-                        }
-                        grade++;
-                    }
-                }
+                data.Push(arr[i]);
             }
-
-            var cnt = arr.Length / k - grade;
-            return list
-                .Where(i => i.Value > cnt)
-                .Select(i => i.Key)
-                .OrderBy(i => i)
-                .ToArray();
+            return data.ToArray();
         }
 
         public static bool Equals(int[] a, int[] b)
@@ -97,7 +59,7 @@ namespace ConsoleApp1
             if (a.Length != b.Length)
                 return false;
             for (int i = 0; i < a.Length; i++)
-            { 
+            {
                 if (a[i] != b[i])
                     return false;
             }
