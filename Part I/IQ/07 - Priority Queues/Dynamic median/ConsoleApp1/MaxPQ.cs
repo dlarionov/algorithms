a85cd263635bf6e45b36768995cbf20faf41506e
dlarionov <denis.larionov@gmail.com>
2017-06-27 18:46:48 +0300

    Dinamic Median

---
+++ Part I/IQ/07 - Priority Queues/Dynamic median/ConsoleApp1/MaxPQ.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class MaxPQ
+    {
+        int[] _arr;
+        int _size;
+
+        public MaxPQ(int campacity)
+        {
+            _arr = new int[campacity];
+        }
+
+        private void Swap(int i, int j)
+        {
+            int x = _arr[i];
+            _arr[i] = _arr[j];
+            _arr[j] = x;
+        }
+
+        private void Up(int i)
+        {
+            while (i > 1 && _arr[i] > _arr[i / 2])
+            {
+                Swap(i, i / 2);
+                i = i / 2;
+            }
+        }
+
+        public void Insert(int x)
+        {
+            if (_size >= _arr.Length)
+                throw new OverflowException();
+
+            _arr[_size] = x;
+            Up(_size);
+            _size++;
+        }
+
+        private void Down(int i)
+        {
+            
+        }
+    }
+}
---
+++ Part I/IQ/07 - Priority Queues/Dynamic median/readme.md
@@ -0,0 +1,3 @@
+## Dynamic median
+
+Design a data type that supports insert in logarithmic time, find-the-median in constant time, and remove-the-median in logarithmic time.
