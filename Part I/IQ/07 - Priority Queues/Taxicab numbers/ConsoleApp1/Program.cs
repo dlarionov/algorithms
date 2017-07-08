83929a33f15974e05c1047613bec50a96b46e07c
Denis <denis.larionov@gmail.com>
2017-07-08 11:38:50 +0300

    Taxicab numbers

--- Part I/IQ/07 - Priority Queues/Taxicab numbers/ConsoleApp1/Program.cs
+++ Part I/IQ/07 - Priority Queues/Taxicab numbers/ConsoleApp1/Program.cs
@@ -8,27 +8,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int n = 100000;
+            int n = 1000000;
 
-            foreach (var i in TaxicabNumbers1(n))
+            foreach (var i in FindTaxicabNumbers(n))
                 Console.WriteLine(i);
 
             Console.ReadKey();
         }
 
-        public static IEnumerable<int> TaxicabNumbers1(int n)
+        public static IEnumerable<int> FindTaxicabNumbers(int n)
         {
-            var list = new List<int>();
+            // m is cubic root of n
+            int m = (int)Math.Floor(Math.Pow(n, (double)1 / 3));
 
-            int max = (int)Math.Floor(Math.Pow(n, (double)1 / 3));
-            for (int i = 1; i <= max; i++)
+            // O(m^2) time and O(m^2) space
+            var list = new List<int>();
+            for (int i = 1; i <= m; i++)
             {
-                for (int j = i + 1; j <= max; j++)
+                for (int j = i; j <= m; j++)
                 {
                     list.Add((int)Math.Pow(i, 3) + (int)Math.Pow(j, 3));
                 }
             }
 
+            // we could sort the list by O(m^2*log(m^2)) time and find duplicates by O(m^2) time
+            // but is is better use System.Linq and GroupBy that gives O(m^2) time in summary
             return list
                 .GroupBy(x => x)
                 .Where(x => x.Count() > 1)
