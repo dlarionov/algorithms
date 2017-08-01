12557a075dd1283545ddeb067d6fef709422f09a
Denis <denis.larionov@gmail.com>
2017-08-01 20:59:30 +0300

    4-SUM

---
+++ Part I/IQ/10 - Hash Tables/4-SUM/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 512;
+            var rnd = new Random();
+            int trueCnt = 0;
+            int falseCnt = 0;
+            int[] arr = new int[n];
+
+            for (int j = 0; j < 1000; j++)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = rnd.Next(int.MinValue, int.MaxValue);
+                }
+
+                if (IsThere4Sum(arr))
+                    trueCnt++;
+                else
+                    falseCnt++;
+            }
+
+            Console.WriteLine($"{trueCnt} / {falseCnt}");
+            Console.ReadKey();
+        }
+
+        public static bool IsThere4Sum(int[] arr)
+        {
+            // O(n^2) time and O(n^2) space
+            var set = new HashSet<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (!set.Add(arr[i] + arr[j]))
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
