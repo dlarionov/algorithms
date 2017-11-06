7446c37726b55ac3f8692fc8d95a97727fc3f656
Denis <denis.larionov@gmail.com>
2017-11-06 08:01:42 +0300

    wordnet

--- Part II/01 - WordNet/src/WordNet.java
+++ Part II/01 - WordNet/src/WordNet.java
@@ -1,16 +1,18 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Graph;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.Topological;
-import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.BreadthFirstPaths;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class WordNet 
 {
-    private Digraph graph;
-    private ArrayList<String> vertices;   
-    private HashMap<String, String> words;
+    private final Graph graph;
+    private final Digraph digraph;
+    private final ArrayList<String> vertices = new ArrayList<String>();   
+    private final HashMap<String, ArrayList<Integer>> words = new HashMap<String, ArrayList<Integer>>();
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms)
@@ -18,12 +20,10 @@ public class WordNet
         if (synsets == null || hypernyms == null)
             throw new java.lang.IllegalArgumentException();
         
-        vertices = new ArrayList<String>();
-        words = new HashMap<String, String>();
-        
         In file = new In(synsets);
         String line = file.readLine();
         String[] arr;
+        ArrayList<Integer> ids;
         int index = 0;
         while(line != null)
         {
@@ -34,21 +34,24 @@ public class WordNet
             {
                 if (words.containsKey(noun))
                 {
-                    String ids = words.get(noun);
-                    ids += "," + Integer.toString(index);
-                    words.put(noun, ids);
+                    ids = words.get(noun);
+                    ids.add(index);
+                    // words.put(noun, ids);
                 }
                 else
                 {
-                    words.put(noun, Integer.toString(index));
-                }
+                    ids = new ArrayList<Integer>();
+                    ids.add(index);
+                    words.put(noun, ids);
+                }                
             }            
             line = file.readLine();
             index++;
         }
         
-        graph = new Digraph(vertices.size());
-        
+        graph = new Graph(index);
+        digraph = new Digraph(index);
+                
         file = new In(hypernyms);
         line = file.readLine();
         while(line != null)
@@ -56,12 +59,13 @@ public class WordNet
             arr = line.split("\\,");
             for(int i = 1; i < arr.length; i++)
             {
-                graph.addEdge(Integer.parseInt(arr[i]), Integer.parseInt(arr[0]));                
+                graph.addEdge(Integer.parseInt(arr[i]), Integer.parseInt(arr[0]));
+                digraph.addEdge(Integer.parseInt(arr[i]), Integer.parseInt(arr[0]));
             }                
             line = file.readLine();
         }
         
-        Topological dag = new Topological(graph);
+        Topological dag = new Topological(digraph);
         if (!dag.hasOrder())
         {
             throw new java.lang.IllegalArgumentException();
@@ -89,68 +93,74 @@ public class WordNet
         if (!isNoun(nounA) || !isNoun(nounA))
             throw new java.lang.IllegalArgumentException();
         
-        String[] a = words.get(nounA).split("\\,");
-        String[] b = words.get(nounA).split("\\,");
+        ArrayList<Integer> idsV = words.get(nounA);
+        ArrayList<Integer> idsW = words.get(nounB);
         
         int min = Integer.MAX_VALUE;
-        for(int i = 0; i < a.length; i++)
+        BreadthFirstPaths paths = new BreadthFirstPaths(graph, idsW);
+        for(int i = 0; i < idsV.size(); i++)
         {
-            for(int j = 0; j < b.length; j++)
-            {
-                min = Math.min(min, distance(i, j));
-            }
-        }       
+            min = Math.min(min, paths.distTo(idsV.get(i)));
+        }
         
         return min;
-    }
-    
-    private int hypernym(int x)
-    {
-        for(int i : graph.adj(x))
-        {
-            return i;
-        }
-        return -1;
-    }
-    
-    private Queue<Integer> path(int x)
-    {
-        Queue<Integer> q = new Queue<Integer>();
-        while(x > 0)
-        {
-            q.enqueue(x);
-            x = hypernym(x);
-        }
-        return q;
-    }
-    
-    private int distance(int v, int w)
-    {
-        if (v == w)
-            return 0;
-        
-        Queue<Integer> pathV = path(v);
-        Queue<Integer> pathW = path(w);
-        
-        while(pathV.size() > 0 && pathW.size() > 0)
-        {
-            if (pathV.dequeue() != pathW.dequeue())
-                break;                      
-        }      
-       
-        return pathV.size() + pathW.size() + 2;
     }
     
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB)
     {
-        return null;
+        if (!isNoun(nounA) || !isNoun(nounA))
+            throw new java.lang.IllegalArgumentException();
+        
+        ArrayList<Integer> idsV = words.get(nounA);
+        ArrayList<Integer> idsW = words.get(nounB);
+        
+        int dist;
+        int min = Integer.MAX_VALUE;
+        Iterable<Integer> path = null;        
+        BreadthFirstPaths paths = new BreadthFirstPaths(graph, idsW);
+        for(int i = 0; i < idsV.size(); i++)
+        {
+            dist = paths.distTo(idsV.get(i));
+            if (min > dist)
+            {
+                min = dist;
+                path = paths.pathTo(i);
+            }
+        }
+        
+        // todo use binary search for finding root in the path
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for(int i : path)
+        {
+            arr.add(i);
+        }    
+        
+        int index = arr.size() - 1;
+        while(index > 0 && contains(digraph.adj(arr.get(index)), arr.get(index-1)))
+        {
+            index--;
+        }
+        
+        return vertices.get(index);
+    }
+    
+    private static Boolean contains(Iterable<Integer> data, int x)
+    {
+        for (int i : data) 
+        {
+            if (x == i) 
+            {
+                return true;
+            }
+        }
+        return false;
     }
     
     // do unit testing of this class
     public static void main(String[] args)
     {
-        WordNet wn = new WordNet("../test/synsets500-subgraph.txt", "../test/hypernyms100-subgraph.txt");
+        WordNet wn = new WordNet("../test/synsets500-subgraph.txt", "../test/hypernyms100-subgraph.txt");    
     }
 }
