28c75fe049ebb0f53be44365e9f95f0c18d9ec5a
Denis <denis.larionov@gmail.com>
2017-11-24 08:39:13 +0300

    started with flow network

--- Part II/03 - Baseball Elimination/BaseballElimination.java
+++ Part II/03 - Baseball Elimination/BaseballElimination.java
@@ -1,24 +1,26 @@
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.FlowNetwork;
+import edu.princeton.cs.algs4.FlowEdge;
 import edu.princeton.cs.algs4.StdOut;
 import java.util.HashMap;
 
 public class BaseballElimination
 {
     private final HashMap<String, Integer> teams;
-    private final int[] wins;
-    private final int[] losses;
-    private final int[] remaining;
-    private final int[][] schedule;
+    private final int[] w;
+    private final int[] l;
+    private final int[] r;
+    private final int[][] s;
     
     public BaseballElimination(String filename) {
         In file = new In(filename);
         String str = file.readLine();
         int len = Integer.parseInt(str);
         
-        wins = new int[len];
-        losses = new int[len];
-        remaining = new int[len];
-        schedule = new int[len][len];
+        w = new int[len];
+        l = new int[len];
+        r = new int[len];
+        s = new int[len][len];
         
         String[] bufer;
         teams = new HashMap<String, Integer>();
@@ -28,12 +30,12 @@ public class BaseballElimination
             
             teams.put(bufer[0], i);
             
-            wins[i] = Integer.parseInt(bufer[1]);
-            losses[i] = Integer.parseInt(bufer[2]);
-            remaining[i] = Integer.parseInt(bufer[3]);            
+            w[i] = Integer.parseInt(bufer[1]);
+            l[i] = Integer.parseInt(bufer[2]);
+            r[i] = Integer.parseInt(bufer[3]);            
             
             for (int j = 0; j < len; j++) {
-                schedule[i][j] = Integer.parseInt(bufer[j+4]);
+                s[i][j] = Integer.parseInt(bufer[j+4]);
             }
             
             // StdOut.println("name: " + bufer[0] + " w: " + bufer[1] + " l: " + bufer[2] + " r: " + bufer[3]);
@@ -54,32 +56,57 @@ public class BaseballElimination
     public int wins(String team) {
         if (team == null || !teams.containsKey(team))
             throw new java.lang.IllegalArgumentException();
-        return wins[teams.get(team)];
+        return w[teams.get(team)];
     }
     
     // number of losses for given team
     public int losses(String team) {
         if (team == null || !teams.containsKey(team))
             throw new java.lang.IllegalArgumentException();
-        return losses[teams.get(team)];
+        return l[teams.get(team)];
     }
     
     // number of remaining games for given team
     public int remaining(String team) {
         if (team == null || !teams.containsKey(team))
             throw new java.lang.IllegalArgumentException();
-        return remaining[teams.get(team)];
+        return r[teams.get(team)];
     }
     
     // number of remaining games between team1 and team2
     public int against(String team1, String team2) {
         if (team1 == null || team2 == null || !teams.containsKey(team1) || !teams.containsKey(team2))
             throw new java.lang.IllegalArgumentException();
-        return schedule[teams.get(team1)][teams.get(team2)];
+        return s[teams.get(team1)][teams.get(team2)];
     }
     
     // is given team eliminated?
     public boolean isEliminated(String team) {
+        if (team == null || !teams.containsKey(team))
+            throw new java.lang.IllegalArgumentException();
+        
+        int idx = teams.get(team);
+        int games = w[idx] + r[idx];
+        
+        int v = teams.size();
+        v = (v * v - v) / 2 + 2;        
+        FlowNetwork nw = new FlowNetwork(v);
+        
+        int s = 0;
+        int t = v - 1;
+        
+        int x = t;
+        for (int i : teams.values()) {
+            if (i == idx) 
+                continue;
+            
+            int f = games - w[i];
+            if (f < 0)
+                return true; // simple elumination
+            
+            nw.addEdge(new FlowEdge(--x, t, f));
+        }
+        
         return false;
     }
     
