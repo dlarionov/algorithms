62f5b63baf2c0c8daa2f3c5e2bfdac523ea2cc83
dlarionov <denis.larionov@gmail.com>
2017-12-05 20:38:53 +0300

    100/100 implemented prefix query operations

--- Part II/04 - Boggle/BoggleSolver.java
+++ Part II/04 - Boggle/BoggleSolver.java
@@ -1,41 +1,94 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
+import java.util.HashSet;
 
 public class BoggleSolver
 {
-    private final BoggleTrie set;
+    private static final int R = 26;
+    private static final int D = 65; // offset of A
+    
+    private final Trie set;
     
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
-        if (dictionary == null) 
+        if (dictionary == null)
             throw new IllegalArgumentException("argument is null");
         
-        set = new BoggleTrie();
+        set = new Trie();
         for (String w : dictionary) {
             set.add(w);
         }
     }
     
+    private static class Node {
+        private Node[] next = new Node[R];
+        private boolean isString;
+    }
+    
+    private class Trie
+    {
+        private Node root;
+        
+        public Node root() {
+            return root;
+        }
+        
+        public Node get(String key) {
+            return get(root, key, 0);
+        }        
+        
+        public Node get(Node x, String key) {
+            return get(x, key, 0);
+        }
+        
+        private Node get(Node x, String key, int d) {
+            if (x == null) return null;
+            if (d == key.length()) return x;
+            int c = key.charAt(d);
+            // if (c < D || c > D + R - 1) return null;
+            return get(x.next[c-D], key, d+1);
+        }
+        
+        public void add(String key) {
+            if (key == null) throw new IllegalArgumentException("argument to add() is null");
+            root = add(root, key, 0);
+        }
+        
+        private Node add(Node x, String key, int d) {
+            if (x == null) x = new Node();
+            if (d == key.length()) {
+                x.isString = true;
+            }
+            else {
+                int c = key.charAt(d);
+                // if (c < D || c > D + R - 1) 
+                //    throw new IllegalArgumentException("argument to add() contains letter that is not in the alphabet");
+                x.next[c-D] = add(x.next[c-D], key, d+1);
+            }
+            return x;
+        }
+    }    
+    
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
         if (board == null) 
-            throw new IllegalArgumentException("argument to getAllValidWords() is null");        
+            throw new IllegalArgumentException("argument to getAllValidWords() is null");
         
-        BoggleTrie words = new BoggleTrie();        
-        for (int i = 0; i < board.cols(); i++) {
-            for (int j = 0; j < board.rows(); j++) {
-                dfs(board, i, j, words);
+        int w = board.cols();
+        int h = board.rows();
+        
+        HashSet<String> words = new HashSet<String>();
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                // todo write an interactive version
+                dfs(board, i, j, new int[2][0], set.root(), "", words);
             }
         }
-        return words.toList();
+        return words;
     }
     
-    private void dfs(BoggleBoard board, int x, int y, BoggleTrie result) {
-        dfs(board, x, y, new int[2][0], "", result);
-    }
-    
-    private void dfs(BoggleBoard board, int x, int y, int[][] path, String prefix, BoggleTrie result) {
+    private void dfs(BoggleBoard board, int x, int y, int[][] path, Node node, String prefix, HashSet<String> result) {
         int w = board.cols();
         int h = board.rows();
         
@@ -47,30 +100,34 @@ public class BoggleSolver
                 return;
         }
         
-        char letter = board.getLetter(y, x);
-        prefix += letter == 'Q' ? letter + "U" : letter;
-        
-        if (!set.hasKeysWithPrefix(prefix))
+        String letter = String.valueOf(board.getLetter(y, x));
+        if (letter.equals("Q")) {
+            letter += "U";            
+        }
+        node = set.get(node, letter);
+        if (node == null)
             return;
         
-        if (prefix.length() > 2 && set.contains(prefix))
+        prefix += letter;
+        if (prefix.length() > 2 && node.isString && !result.contains(prefix))
             result.add(prefix);
         
+        // todo optimize path processing
         int[][] copy = new int[2][path[0].length + 1];
         System.arraycopy(path[0], 0, copy[0], 0, path[0].length);
         System.arraycopy(path[1], 0, copy[1], 0, path[1].length);
         copy[0][copy[0].length-1] = x;
         copy[1][copy[1].length-1] = y;
         
-        dfs(board, x-1, y-1, copy, prefix, result);
-        dfs(board, x, y-1, copy, prefix, result);
-        dfs(board, x+1, y-1, copy, prefix, result);
-        dfs(board, x-1, y, copy, prefix, result);
-        dfs(board, x+1, y, copy, prefix, result);
-        dfs(board, x-1, y+1, copy, prefix, result);
-        dfs(board, x, y+1, copy, prefix, result);
-        dfs(board, x+1, y+1, copy, prefix, result);        
-    }    
+        dfs(board, x-1, y-1, copy, node, prefix, result);
+        dfs(board, x, y-1, copy, node, prefix, result);
+        dfs(board, x+1, y-1, copy, node, prefix, result);
+        dfs(board, x-1, y, copy, node, prefix, result);
+        dfs(board, x+1, y, copy, node, prefix, result);
+        dfs(board, x-1, y+1, copy, node, prefix, result);
+        dfs(board, x, y+1, copy, node, prefix, result);
+        dfs(board, x+1, y+1, copy, node, prefix, result);
+    }
     
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
@@ -78,26 +135,27 @@ public class BoggleSolver
         if (word == null) 
             throw new IllegalArgumentException("argument to scoreOf() is null");
         
-        if (set.contains(word)) {
-            switch (word.length()) {
-                case 0: return 0;
-                case 1: return 0;
-                case 2: return 0;
-                case 3: return 1;
-                case 4: return 1;
-                case 5: return 2;
-                case 6: return 3;
-                case 7: return 5;
-                default: return 11;
-            }
+        Node x = set.get(word);
+        if (x == null || !x.isString) 
+            return 0;
+        
+        switch (word.length()) {
+            case 0:
+            case 1:
+            case 2: return 0;
+            case 3:
+            case 4: return 1;
+            case 5: return 2;
+            case 6: return 3;
+            case 7: return 5;
+            default: return 11;
         }
-        return 0;
     }
     
     public static void main(String[] args) {
         In in = new In(args[0]);
         String[] dictionary = in.readAllStrings();
-        BoggleSolver solver = new BoggleSolver(dictionary);        
+        BoggleSolver solver = new BoggleSolver(dictionary);
         BoggleBoard board = new BoggleBoard(args[1]);
         int score = 0;       
         for (String word : solver.getAllValidWords(board)) {
